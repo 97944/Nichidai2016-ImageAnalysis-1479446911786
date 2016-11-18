d322177c17ffc87ab825def50d6716d245ccf0ba
二宮 力也 <97944@bsp.co.jp>
2016-11-18 15:50:06 +0900

    second commit

--- Client/Nichidai2016-ImageAnalysis/src/app/app.component.ts
+++ Client/Nichidai2016-ImageAnalysis/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+var watson = require('watson-developer-cloud');
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = '日大特別講義2016 画像解析';
+  private alc: any;
+  
+  private parameters = {
+    url: 'http://www.charliechaplin.com/en/synopsis/articles/29-The-Great-Dictator-s-Speech'
+  };
+  
+  private watson(): void{
+    this.alc = watson.alchemy_language({
+        api_key: '25d7ba025058cc03b15c7ceaaec6015fb5ff5322'
+      });
+      this.alc.emotion(this.parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+  }
+
 }
